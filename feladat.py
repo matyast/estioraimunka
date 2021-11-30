075467f4462150f47b36648aebaf62187012b5b9
Tanuló 301-05 <301-05@KKSZKI.LOCAL>
2021-11-30 18:55:45 +0100

    Kész 4 feladat

---
+++ feladat.py
@@ -0,0 +1,32 @@
+## Első feladat
+for i in range(1,10):
+    print(1/i)
+
+## Második feladat
+hatvany=int(input("Kérem a hatvány alapot:"))
+kitevo=int(input("Kérem a hatvány kitevőt:"))
+hatvanyertek=(hatvany**kitevo)
+print(hatvanyertek)
+
+## Harmadik feladat
+while True:
+    szam=int(input("Kérek egy pozitív számot: "))
+    if szam<=0:
+        print("Nem tudsz olvasni?? Pozitív szám!")
+    else:
+        print("Ügyes vagy!")
+        break
+
+## Negyedik feladat
+a=int(input("Kérem az első számot: "))
+b=int(input("Kérek a második számot: "))
+
+if a>b:
+    print("A két szám közötti távolság: "+str(-(b-a)))
+else:
+    print("A két szám közötti távolság: "+str(-(a-b)))
+
+## Negyedik feladat
+
+
+
